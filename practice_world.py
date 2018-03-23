50f489032b844bd411b46710bfbc4c6c6d86bb6a
Mazen Zawaideh <mzawaide@gmail.com>
2018-03-22 21:23:09 -0700

    added a few lines of code while teaching patty how to write functions

--- practice_world.py
+++ practice_world.py
@@ -1,1 +1,20 @@
 print("just practicing how to commit to github")
+
+print("hello patty! let us learn to code together")
+
+print("hello moto")
+
+#let me teach you how write a function
+
+def patty_add(x, y):
+    answer = x + y
+    return answer
+
+z=patty_add(7,11)
+print(z)
+
+def maz_subtract(x,y):
+    answer = y - x
+    return answer
+
+print(maz_subtract(5,10))    
